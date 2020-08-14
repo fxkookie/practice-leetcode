b980c69afd834c57891ed341d0c4ef9f5fe9072f
TNK <fxkookie@gmail.com>
2020-08-14 17:19:27 +0800

    fix lonest probelm

--- Solution.java
+++ Solution.java
@@ -1,31 +1,20 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-    int poolDepth;
-    int[] candidates;
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-      poolDepth = candidates.length;
-      this.candidates = candidates;
       List<Integer> temp = new ArrayList<Integer>();
-
+      run(temp,0,candidates.length-1,target,candidates);
+      return ans;
     }
-    public void run(List<Integer> res,int sum,int index){
+    public void run(List<Integer> res,int sum,int index,int target,int[] candidates){
       if(sum == target){
         ans.add(res);
       }
       for(int i = index;i>= 0;i--){
         if(sum + candidates[i] <= target){
           res.add(candidates[i]);
-          run(res,sum+candidates[i],i);
+          run(res,sum+candidates[i],i,target,candidates);
           res.remove(res.size()-1);
         }
       }
-
-    }
-
-
-    public static void public static void main(String[] args) {
-      int candidate[] = {2,3,6,7};
-      int target = 7;
-
     }
 }
